bcc5d5c8916acd0646917fbeab72c85c3804a183
D.V.D <dvd@dogada.org>
2018-02-15 13:22:36 +0200

    feat: add Swagger UI docker image and config

--- api/app.js
+++ api/app.js
@@ -10,14 +10,23 @@ var config = {
 
 SwaggerExpress.create(config, function (err, swaggerExpress) {
   if (err) { throw err }
-
+  // add CORS headers to /v\d+/swagger url allow fetch from Swagger UI domain
+  app.get('/v(:?\\d+)/swagger', enableCORS)
   // install middleware
   swaggerExpress.register(app)
 
   var port = process.env.PORT || 10010
   app.listen(port)
-
   if (swaggerExpress.runner.swagger.paths['/transactions']) {
     console.log(`Listening on port ${port}...`)
   }
 })
+
+// add required CORS headers
+function enableCORS (request, response, next) {
+  response.header('Access-Control-Allow-Origin', '*')
+  response.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS')
+  response.header('Access-Control-Allow-Headers', 'Accept, Origin, Content-Type')
+  response.header('Access-Control-Allow-Credentials', 'true')
+  next()
+}
