672282ab8a2363e41e47f39b0267a047a8bb96fb
D.V.D <dvd@dogada.org>
2018-02-16 20:46:42 +0200

    feat: catch all errors in asyncController

--- api/api/helpers/async.js
+++ api/api/helpers/async.js
@@ -1,9 +1,22 @@
+function errorHandler (res) {
+  return e => {
+    res.status(500)
+    res.json({
+      message: e.message || 'Unknown error'
+    })
+  }
+}
+
 /**
  * Workaround for swagger-node that doesn't support async controllers yet.
  * @param {function} handler - function that accepts (req, res) and returns a promise
  */
 function asyncController (handler) {
-  return (req, res) => handler(req, res).then()
+  return (req, res) => {
+    handler(req, res)
+      .then() // if there is no error handler already sent data
+      .catch(errorHandler(res))
+  }
 }
 
 module.exports = {
