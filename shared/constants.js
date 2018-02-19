295a26cb0554d28653b87a2ecc710a56417011ce
D.V.D <dvd@dogada.org>
2018-02-19 10:39:52 +0200

    feat: configure access to the API

--- shared/constants.js
+++ shared/constants.js
@@ -2,3 +2,5 @@ export const PROJECT_ID = 'microchain'
 export const NS = PROJECT_ID
 export const DRAWER_WIDTH = 240
 export const DEV_MODE = (process.env.NODE_ENV === 'development')
+// latest API version
+export const API_ROOT = 'http://localhost:10010/v1/'
---
+++ webapp/api/fetchJson.js
@@ -0,0 +1,42 @@
+// @flow
+
+import HttpError from '~/utils/HttpError'
+import {API_ROOT} from '~/shared/constants'
+
+function parseJsonResponse (res) {
+  var contentType = res.headers.get('content-type')
+  if (contentType && contentType.includes('application/json')) {
+    return res.json()
+  }
+  throw new HttpError(400, "Oops, we haven't got JSON!")
+}
+
+function parseResponse (res) {
+  return parseJsonResponse(res).then(data => {
+    // console.log('data', data)
+    if (res.ok) return data
+    throw new HttpError(res.status, data.error || res.statusText)
+  })
+}
+
+export default function fetchApi (url: string, opts?: Object) {
+  if (typeof window === 'undefined') {
+    console.error('No window on server')
+    return Promise.reject(new Error('No window'))
+  }
+  opts = {
+    ...opts,
+    credentials: 'same-origin', // send cookies
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }
+  }
+  if (typeof opts.body === 'object') {
+    opts.body = JSON.stringify(opts.body)
+  }
+
+  return window
+    .fetch(`${API_ROOT}${url}`, opts)
+    .then(parseResponse)
+}
