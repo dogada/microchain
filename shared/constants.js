c3daddd5d560d775738faf59ec4c4f177dd7d962
D.V.D <dvd@dogada.org>
2018-02-21 23:46:54 +0200

    fix(webapp): temp workaround for runtime vars inside webpack bundle

--- shared/constants.js
+++ shared/constants.js
@@ -1,7 +1,32 @@
+// FIXME: bridge process.env to WebPack
+// https://github.com/zeit/next.js/issues/1103
+// or populate window.APP_ENV from process.env on server
+let ENV = 'prod'
+if (typeof window !== 'undefined') {
+  ENV = {
+    'localhost:3000': 'dev',
+    'localhost:3001': 'qa'
+  }[window.location.host] || 'prod'
+}
+
+const CONFIG = {
+  dev: {
+    API_ROOT: 'http://localhost:10010/v1/',
+    MINER_ACCOUNT: 'DEV_MINER'
+  },
+  qa: {
+    API_ROOT: 'http://localhost:10011/v1/',
+    MINER_ACCOUNT: 'QA_MINER'
+  },
+  prod: {
+    API_ROOT: 'https://api.mic.dogada.org/v1/',
+    MINER_ACCOUNT: 'PROD_MINER'
+  }
+}
+
 export const PROJECT_ID = 'microchain'
 export const NS = PROJECT_ID
 export const DRAWER_WIDTH = 240
 export const DEV_MODE = (process.env.NODE_ENV === 'development')
-// latest API version
-export const API_ROOT = 'http://localhost:10010/v1/'
-export const MINER_ACCOUNT = 'MINER_ACCOUNT'
+export const API_ROOT = CONFIG[ENV].API_ROOT
+export const MINER_ACCOUNT = CONFIG[ENV].MINER_ACCOUNT || 'MINER_ACCOUNT'
